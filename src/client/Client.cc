5843c7e8bed53f7ba17b9d77397a607e60c6756c
Sidharth Anupkrishnan <sanupkri@redhat.com>
2020-06-23 17:17:26 +0530

    client: Fix setxattr with 0 size specified
    When xattrs are set with 0 size, the value should be set to an empty string.
    Signed-off-by: Sidharth Anupkrishnan <sanupkri@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -11584,8 +11584,10 @@ int Client::_do_setxattr(Inode *in, const char *name, const void *value,
 {
 
   int xattr_flags = 0;
-  if (!value)
-    xattr_flags |= CEPH_XATTR_REMOVE;
+  // If the size is set to 0, set the value to an empty string
+  if (size == 0) {
+    value = "";
+  }
   if (flags & XATTR_CREATE)
     xattr_flags |= CEPH_XATTR_CREATE;
   if (flags & XATTR_REPLACE)
@@ -11640,7 +11642,6 @@ int Client::_setxattr(Inode *in, const char *name, const void *value,
 	if (ret < 0)
 	  return ret;
 	if (ret == 0) {
-	  value = NULL;
 	  size = 0;
 	}
 	if (new_mode != in->mode) {
@@ -11659,7 +11660,6 @@ int Client::_setxattr(Inode *in, const char *name, const void *value,
 	if (ret < 0)
 	  return -EINVAL;
 	if (ret == 0) {
-	  value = NULL;
 	  size = 0;
 	}
       }
